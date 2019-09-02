a5ebbfcb9f9111b54a8aaa72be2b8540f58981a2
Alexander Makarof <makarofalex@gmail.com>
2019-09-02 22:19:00 +0300

    'save'

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -62,6 +62,7 @@ export default class App extends Component {
 						<ItemList
 							onItemSelected={this.onPersonSelected}
 							getData={this.swapiService.getAllPlanets}
+							renderItem={(item) => item.name}
 						/>
 					</div>
 					<div className="col-md-6">
