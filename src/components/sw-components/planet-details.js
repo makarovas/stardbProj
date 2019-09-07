8d970367ec56055953b047b5979d8f57cb3d43ac
Alexander Makarof <makarofalex@gmail.com>
2019-09-07 15:46:09 +0300

    'mapsFromPRops-planet-details'

--- src/components/sw-components/planet-details.js
+++ src/components/sw-components/planet-details.js
@@ -3,30 +3,29 @@ import React from 'react';
 import ItemDetails, { Record } from '../item-details';
 
 import { SwapiServiceConsumer } from '../swapi-service-context';
+import { withSwapiService } from '../hoc-helpers';
+import SwapiService from '../../services/swapi-service';
+
 
 const PlanetDetails = ({ itemId }) => {
 	return (
+		<ItemDetails
+			itemId={itemId}
+			getData={getPlanet}
+			getImageUrl={getPlanetImage}>
 
-		<SwapiServiceConsumer>
-			{
-				({ getPlanet, getPlanetImage }) => {
-					return (
-						<ItemDetails
-							itemId={itemId}
-							getData={getPlanet}
-							getImageUrl={getPlanetImage}>
-
-							<Record field="population" label="Population" />
-							<Record field="rotationPeriod" label="Rotation Period" />
-							<Record field="diameter" label="Diameter" />
-						</ItemDetails>
-					);
-				}
-			}
-		</SwapiServiceConsumer>
+			<Record field="population" label="Population" />
+			<Record field="rotationPeriod" label="Rotation Period" />
+			<Record field="diameter" label="Diameter" />
+		</ItemDetails>
 	);
 };
 
+const mapMethodsToProps = ({ getPlanet, getPlanetImage }) => {
+	return {
+		getData: getPlanet,
+		getImageUrl: getPlanetImage
+	}
+}
 
-
-export default PlanetDetails;
+export default withSwapiService(PlanetDetails, mapMethodsToProps);
