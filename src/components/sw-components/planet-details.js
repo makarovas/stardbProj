d1962a35c4d6e34dab6d16d0be9594261d70cd66
Alexander Makarof <makarofalex@gmail.com>
2019-09-10 21:59:45 +0300

    'fix-bug'

--- src/components/sw-components/planet-details.js
+++ src/components/sw-components/planet-details.js
@@ -8,16 +8,16 @@ const PlanetDetails = (props) => {
       <Record field="population" label="Population" />
       <Record field="rotationPeriod" label="Rotation Period" />
       <Record field="diameter" label="Diameter" />
-     </ItemDetails>
-    );
-      
-
-const mapMethodsToProps = ({getPlanet, getPlanetImage}) => {
-    return {
-      getData: getPlanet,
-    getImageUrl: getPlanetImage
-   };
+    </ItemDetails>
+  );
 }
-  
 
-  export default withSwapiService(PlanetDetails, mapMethodsToProps);  
+const mapMethodsToProps = ({ getPlanet, getPlanetImage }) => {
+  return {
+    getData: getPlanet,
+    getImageUrl: getPlanetImage
+  };
+}
+
+
+export default withSwapiService(PlanetDetails, mapMethodsToProps);  
