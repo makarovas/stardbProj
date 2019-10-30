ce40c1c8ea51147066f6f0b627db4524c8514b68
Alexander Makarof <makarofalex@gmail.com>
2019-10-30 21:24:46 +0300

    cntnus refactoring

---
+++ src/components/pages/peaple-page.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react'
+
+export default class PeoplePage extends Component {
+	state = {
+		selectedItem: null
+	}
+
+	render() {
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
